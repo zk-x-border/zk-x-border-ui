53b0f843b0a951a6acfdd3ac99ca6a7ec06a250a
Justin Chen <justin@setprotocol.com>
2023-07-22 01:31:20 +0200

    feat: update upload feature

--- src/components/payForm/PayFormStep3.tsx
+++ src/components/payForm/PayFormStep3.tsx
@@ -1,24 +1,48 @@
+import { useCallback, useState } from 'react';
 import Dropzone from 'react-dropzone';
+import { GrCloudUpload, GrDocument } from 'react-icons/gr';
 
 export type PayFormStep3Props = {
   onClickNext: () => void;
 };
 
 const PayFormStep3: React.FC<PayFormStep3Props> = ({ onClickNext }) => {
+  const [acceptedFileNames, setAcceptedFileNames] = useState<string[]>([]);
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setAcceptedFileNames(acceptedFiles.map((file) => file.name));
+  }, []);
+
   return (
     <div>
-      <Dropzone accept={{ 'text/*': ['.eml'] }} multiple={false}>
+      <div className='mb-4 rounded-lg bg-gray-700 px-20 py-6 text-white'>
+        Please upload the email to demonstrate proof of payment
+      </div>
+      <Dropzone
+        onDropAccepted={onDrop}
+        accept={{ '*': ['.eml'] }}
+        multiple={false}
+      >
         {({ getRootProps, getInputProps }) => (
           <div
             {...getRootProps()}
-            className='relative flex flex-1 justify-center space-x-2 rounded-lg border border-gray-300 px-6 py-40'
+            className='relative flex flex-1 justify-center space-x-2 rounded-lg border border-gray-300 bg-gray-500/50 px-6 py-40'
           >
             <input {...getInputProps()} />
             <section>
               <div>
-                <p className='text-white'>
-                  Drag 'n' drop .eml file here, or click to select file
-                </p>
+                {acceptedFileNames?.length ? (
+                  <div className='flex flex-1 items-center justify-center space-x-4'>
+                    <GrDocument size={32} />{' '}
+                    <p className='text-white'>{acceptedFileNames[0]}</p>
+                  </div>
+                ) : (
+                  <div className='flex flex-col items-center justify-center space-y-2'>
+                    <GrCloudUpload size={48} />
+                    <p className='text-white'>
+                      Drag 'n' drop .eml file here, or click to select file
+                    </p>
+                  </div>
+                )}
               </div>
             </section>
           </div>
